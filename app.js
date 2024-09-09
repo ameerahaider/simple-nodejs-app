1f8a1304ea817a6dfa63d89febd2137775fef8f9
Ameera Haider Rizvi <77534392+ameerahaider@users.noreply.github.com>
2024-09-09 23:57:09 +0500

    Update app.js

--- app.js
+++ app.js
@@ -27,6 +27,6 @@ app.use((req, res) => {
 
 // Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
